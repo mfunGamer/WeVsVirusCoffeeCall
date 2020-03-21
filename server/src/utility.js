4c516dbdeeb3f3624193a812c0957a08e445520a
rhueber <rc.hueber@gmail.com>
2020-03-21 12:41:43 +0100

    Removed unnecessary logging.

--- server/src/utility.js
+++ server/src/utility.js
@@ -1,9 +1,5 @@
 function requireParameters(params, req){
-    console.log(params)
-    console.log(req.query)
     for(let i=0; i < params.length; i++){
-        console.log(!(params[i] in req.query))
-        console.log(params[i])
         if (!(params[i] in req.query)){
             return params[i];
         }
