02e2b5089f6697124dc936d5fd84a7f7cf379cf7
Neal <n.asprion@gmx.de>
2020-03-21 17:40:53 +0100

    First Map implementation

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,18 +8,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import {HomePage} from "./home/home.page";
-import {SettingsPage} from "./settings/settings.page";
-import {RewardHistoryPage} from "./reward-history/reward-history.page";
-import {LegalNoticePage} from "./legal-notice/legal-notice.page";
-import {DataProtectionPage} from "./data-protection/data-protection.page";
-import {CreateAccountPage} from "./create-account/create-account.page";
-import {HomePageModule} from "./home/home.module";
-import {SettingsPageModule} from "./settings/settings.module";
-import {RewardHistoryPageModule} from "./reward-history/reward-history.module";
-import {LegalNoticePageModule} from "./legal-notice/legal-notice.module";
-import {DataProtectionPageModule} from "./data-protection/data-protection.module";
-import {CreateAccountPageModule} from "./create-account/create-account.module";
+import {HomePage} from './home/home.page';
+import {SettingsPage} from './settings/settings.page';
+import {RewardHistoryPage} from './reward-history/reward-history.page';
+import {LegalNoticePage} from './legal-notice/legal-notice.page';
+import {DataProtectionPage} from './data-protection/data-protection.page';
+import {CreateAccountPage} from './create-account/create-account.page';
+import {HomePageModule} from './home/home.module';
+import {SettingsPageModule} from './settings/settings.module';
+import {RewardHistoryPageModule} from './reward-history/reward-history.module';
+import {LegalNoticePageModule} from './legal-notice/legal-notice.module';
+import {DataProtectionPageModule} from './data-protection/data-protection.module';
+import {CreateAccountPageModule} from './create-account/create-account.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -42,7 +43,9 @@ import {CreateAccountPageModule} from "./create-account/create-account.module";
       RewardHistoryPageModule,
       LegalNoticePageModule,
       DataProtectionPageModule,
-      CreateAccountPageModule
+      CreateAccountPageModule,
+
+      HttpClientModule,
   ],
 
   providers: [
--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Map, tileLayer, marker, icon } from 'leaflet';
+import { map } from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +12,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public http: HttpClient,
+              public plt: Platform,
+              public router: Router) {
 
-  exampleMethod(){
-    alert("Ola");
+  }
+
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngAfterViewInit() {
+    this.initMap();
+    this.plt.ready().then(() => {
+      this.http.get('https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev')
+          .pipe(map(res => res))
+          .subscribe(restaurants => console.log(restaurants));
+    });
+  }
+
+  initMap() {
+
+    // tslint:disable-next-line:no-shadowed-variable
+    const map = new Map('map').setView([33.6396965, -84.4304574], 23);
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const customMarkerIcon = icon({
+      iconUrl: 'assets/images/dish_02.png',
+      iconSize: [64, 64],
+      popupAnchor: [0, -20]
+    });
+
+    const restaurants = [{id: 3, lat: 33.6396965, lng: -84.4304574}];
+
+    restaurants.forEach((restaurant) => {
+      marker([restaurant.lat, restaurant.lng], {icon: customMarkerIcon})
+          .bindPopup(`<b>${restaurant.id}</b>`, { autoClose: false })
+          .on('click', () => this.router.navigateByUrl('/restaurant'))
+          .addTo(map).openPopup();
+    });
+
+    setTimeout(() => { map.invalidateSize(); }, 500);
   }
 
 }
