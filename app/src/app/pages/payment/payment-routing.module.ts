7b1beee530a3ba72bb0dad464aef8956d12d39d9
Felix Ortmann <f.ortmann09@t-online.de>
2020-03-21 20:00:10 +0100

    Added basic payment method with paypal

--- app/src/app/pages/payment/payment-routing.module.ts
+++ app/src/app/pages/payment/payment-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PaymentPage } from './payment.page';
+import {PayPal} from '@ionic-native/paypal/ngx';
 
 const routes: Routes = [
   {
@@ -13,5 +14,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
+  providers: [PayPal]
 })
 export class PaymentPageRoutingModule {}
--- app/src/app/pages/payment/payment.page.ts
+++ app/src/app/pages/payment/payment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PayPal, PayPalPayment, PayPalConfiguration} from '@ionic-native/paypal/ngx';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentPage implements OnInit {
 
-  constructor() { }
+  constructor(private payPal: PayPal) { }
 
   ngOnInit() {
+  }
+
+  pay() {
+    console.log('Bezahlen');
+    this.payPal.init( {
+      PayPalEnvironmentProduction: '',
+      PayPalEnvironmentSandbox: 'ATrKos7gH-abX4fJ5CmkrByTvT9bxIYNBdsKNfceJ06_9nKvIvwm8cPPB6dzUBrjknAkFnbxhWLi8vgi'
+    }).then(() => {
+      this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration( {
+        acceptCreditCards: false,
+        languageOrLocale: 'de-de',
+        merchantName: 'WeLoveLocal',
+        merchantPrivacyPolicyURL: '',
+        merchantUserAgreementURL: ''
+      })).then(() => {
+        const payment = new PayPalPayment('3.99', 'EUR', 'WeLL', 'Sale');
+        this.payPal.renderSinglePaymentUI(payment).then(() => {
+          console.log('Payment successful');
+        }, () => {
+          console.log('Render error');
+        });
+      }, () => {
+
+      });
+    }, () => {
+
+    });
   }
 
 }
